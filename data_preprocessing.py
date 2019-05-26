5beb5e803779a5a787f4dcba169bf01b6fbee8e8
William Bakst <wbakst@stanford.edu>
2019-05-25 19:03:08 -0700

    fixed mistake in preprocessing, files are now correct

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,3 +1,5 @@
+import collections
+
 ########################################
 ############ DATA VARIABLES ############
 ########################################
@@ -45,16 +47,17 @@ SPLITS = [
 ########################################
 
 vocabulary = set()
-examples = []
+examples = collections.defaultdict(list)
 
 for domain in DOMAINS:
     for label_type in LABEL_TYPE:
         for split in SPLITS:
             filename = './data/{}.{}.{}'.format(domain, label_type, split)
+            key = './preprocessed_data/{}.{}.{}'.format(domain, label_type, split)
             with open(filename, 'r') as file:
                 for line in file:
                     line = line.split()
-                    examples.append(line)
+                    examples[key].append(line)
                     for word in line[:-1]: 
                         if word.isalpha(): vocabulary.add(word)
 
@@ -72,24 +75,14 @@ with open(filename, 'w') as out:
     for word, wid in VOCAB_IDS.items():
         print('{} {}'.format(word, wid), file=out)
 
-########################################
-######## CONSTRUCT ID EXAMPLES #########
-########################################
+###########################################
+# CONSTRUCT ID EXAMPLES AND WRITE TO FILE #
+###########################################
 
-new_examples = []
-for ex in examples:
-    words, label = ex[:-1], ex[-1]
-    new_ex = [VOCAB_IDS[word] if word in VOCAB_IDS else VOCAB_IDS['UNK'] for word in words] + [label]
-    new_examples.append(new_ex)
-
-########################################
-########## SAVE NEW EXAMPLES ###########
-########################################
-
-for domain in DOMAINS:
-    for label_type in LABEL_TYPE:
-        for split in SPLITS:
-            filename = './preprocessed_data/{}.{}.{}'.format(domain, label_type, split)
-            with open(filename, 'w') as out:
-                for ex in new_examples:
-                    print(' '.join([str(i) for i in ex]), file=out)
+new_examples = collections.defaultdict(list)
+for filename, reviews in examples.items():
+    with open(filename, 'w') as out:
+        for ex in reviews:
+            words, label = ex[:-1], ex[-1]
+            new_ex = [VOCAB_IDS[word] if word in VOCAB_IDS else VOCAB_IDS['UNK'] for word in words] + [label]
+            print(' '.join([str(i) for i in new_ex]), file=out)
