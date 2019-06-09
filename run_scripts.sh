f1cb96fbcdceae835b130aa329708a6f7e8e2688
William Bakst <wbakst@stanford.edu>
2019-06-09 04:39:01 -0700

    updated shell script

--- run_scripts.sh
+++ run_scripts.sh
@@ -1,6 +1,6 @@
-python main.py --use_gpu --num_epochs=1000
+python main.py --use_gpu
 
-python bert_main.py --use_gpu --num_epochs=1000
+python bert_main.py --use_gpu
 
 python pytorch-pretrained-BERT/examples/run_classifier_few_shot.py --do_train --do_eval
 
