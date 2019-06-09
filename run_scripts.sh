85efe73e1e96cd41de83568f36b1d4903b6bb84e
William Bakst <wbakst@stanford.edu>
2019-06-09 04:01:33 -0700

    updated gpu issue and added shell script

---
+++ run_scripts.sh
@@ -0,0 +1,7 @@
+python main.py --use_gpu
+
+python bert_main.py --use_gpu
+
+python pytorch-pretrained-BERT/examples/run_classifier_few_shot.py --do_train --do_eval
+
+python pytorch-pretrained-BERT/examples/run_classifier_standard.py --do_train --do_eval
